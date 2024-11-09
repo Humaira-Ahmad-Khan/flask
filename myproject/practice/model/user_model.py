b50a9f90fcfdf07d5ca1f083de9113c413854a90
Humaira <digitalartistctn@gmail.com>
2024-11-09 23:11:58 +0500

    new

--- myproject/practice/model/user_model.py
+++ myproject/practice/model/user_model.py
@@ -1,3 +1,20 @@
+import mysql.connector
 class Tea():
+    def __init__(self):
+        try:
+            self.con = mysql.connector.connect(host='localhost' , user='root' , password='qwerty.' , database='flask_learing' )
+            self.cur = self.con.cursor(dictionary=True)
+                       
+            print('connection is connected')
+        except:
+            print('connection is failed')           
+        
+    
     def humaira(self):
-        return"This is my world"
+        self.cur.execute("SELECT * FROM users")
+        result = self.cur.fetchall()
+        
+        return result
+    
+    
+     
